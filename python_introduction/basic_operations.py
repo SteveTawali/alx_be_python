7b54e9d1e102a95623a73cf4a950232f74f28c2c
Steven Gulu <stevenkatawali@gmail.com>
2024-09-12 22:26:13 +0300

    Perfoming basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#Define two numbers
+number1 = 10
+number2 = 5
+
+#Perfom Basic operations
+addition = (number1 + number2)
+subtraction = (number1 - number1)
+multiplication = (number1 * number2)
+
+#Output
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
