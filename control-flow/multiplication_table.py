193920924f61827b800c1b7937853d9cb773a519
Steven Gulu <stevenkatawali@gmail.com>
2024-09-21 15:22:24 +0300

    Add multiplication file

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,7 @@
+number = float(input("Enter a number to see its multiplication table:"))
+
+print(f"Multiplication tabel for {number}:")
+
+for i in range(1, 11):
+    product = number * i
+    print(f"{number} * {i} = {product}")
